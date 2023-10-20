89280cd7928f1585e05e845e4a217a11ef459e45
Jonathan Song <jonats10@uci.edu>
2023-10-20 11:39:20 -0700

    add logic for tester and handle sibmit functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,12 @@ window.addEventListener("load" , () => {
   form.addEventListener("submit" , tester)
 })
 
+function handleSubmit () {
+
+}
+
 
 function tester (e) {
-  e.preventDefault()
   let result;
   const webDev = document.querySelector(".web-dev").value
   const specWebDev = document.querySelector(".specific-webdev").value
@@ -16,10 +19,21 @@ function tester (e) {
   if (webDev && specWebDev && speed && memory && portability) {
     if(webDev === 'yes') {
       const result = "You should learn JavaScript to become a web developer!"
-    } else if{
-      
+    } else if (specWebDev === "platform"){
+      const result = "You are interested in Java! Games in larger platforms use Java."
     }
+      else if (speed === "yes") {
+      const result = "Look into C++, many say that C++ is one of the fastest programming languages!"
+      }
+      else if (memory === "yes") {
+      const result = "A language known for its ability to manage memory and is gaining popularity is called Rust! Look into it."
+      }
+      else if (portability === "yes") {
+      const result = "Java is also a programming language known for portability"
+      }
+      
   } else {
     console.log(error)
   }
+  
 }
