6a0e186c1db0aab3aa5bce97db29d5f66943479f
Jonathan Song <jonats10@uci.edu>
2023-10-20 10:29:09 -0700

    set up javascript for inputs

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+window.addEventListener("load" , () => {
+  const form = document.querySelector(".form")
+  form.addEventListener("submit" , tester)
+})
+
+
+function tester (e) {
+  e.preventDefault()
+  
+}
