03d9ad2afe3bf13eef81fecdff9560c215730723
Jonathan Song <jonats10@uci.edu>
2023-10-20 11:59:42 -0700

    WIP tester and handle submit logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,36 @@
-window.addEventListener("load" , () => {
-  const form = document.querySelector(".form")
-  form.addEventListener("submit" , tester)
-})
+window.addEventListener("load", () => {
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", handleSubmit);
+});
 
-function handleSubmit () {
-
+function handleSubmit(e) {
+  e.preventDefault();
+  let result = ''
+  result = tester();
+  console.log(result)
+  document.querySelector(".result").innerHTML = result;
 }
 
-
-function tester (e) {
-  let result;
-  const webDev = document.querySelector(".web-dev").value
-  const specWebDev = document.querySelector(".specific-webdev").value
-  const speed = document.querySelector(".speed").value
-  const memory = document.querySelector(".memory").value
-  const portability = document.querySelector(".portability").value
+function tester() {
+  const webDev = document.querySelector(".web-dev").value;
+  const specWebDev = document.querySelector(".specific-webdev").value;
+  const speed = document.querySelector(".speed").value;
+  const memory = document.querySelector(".memory").value;
+  const portability = document.querySelector(".portability").value;
 
   if (webDev && specWebDev && speed && memory && portability) {
-    if(webDev === 'yes') {
-      const result = "You should learn JavaScript to become a web developer!"
-    } else if (specWebDev === "platform"){
-      const result = "You are interested in Java! Games in larger platforms use Java."
+    if (webDev === "yes") {
+      return "You should learn JavaScript to become a web developer!";
+    } else if (specWebDev === "platform") {
+      return "You are interested in Java! Games in larger platforms use Java.";
+    } else if (speed === "yes") {
+      return "Look into C++, many say that C++ is one of the fastest programming languages!";
+    } else if (memory === "yes") {
+      return "A language known for its ability to manage memory and is gaining popularity is called Rust! Look into it.";
+    } else if (portability === "yes") {
+      return "Java is also a programming language known for portability";
     }
-      else if (speed === "yes") {
-      const result = "Look into C++, many say that C++ is one of the fastest programming languages!"
-      }
-      else if (memory === "yes") {
-      const result = "A language known for its ability to manage memory and is gaining popularity is called Rust! Look into it."
-      }
-      else if (portability === "yes") {
-      const result = "Java is also a programming language known for portability"
-      }
-      
   } else {
-    console.log(error)
+    console.log(error);
   }
-  
 }
