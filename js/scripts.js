540a1cf18389c4cd9f481ed138b48492db0e77d8
Jonathan Song <jonats10@uci.edu>
2023-10-20 11:00:23 -0700

    connect script to values of input

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,20 @@ window.addEventListener("load" , () => {
 
 function tester (e) {
   e.preventDefault()
-  
+  let result;
+  const webDev = document.querySelector(".web-dev").value
+  const specWebDev = document.querySelector(".specific-webdev").value
+  const speed = document.querySelector(".speed").value
+  const memory = document.querySelector(".memory").value
+  const portability = document.querySelector(".portability").value
+
+  if (webDev && specWebDev && speed && memory && portability) {
+    if(webDev === 'yes') {
+      const result = "You should learn JavaScript to become a web developer!"
+    } else if{
+      
+    }
+  } else {
+    console.log(error)
+  }
 }
